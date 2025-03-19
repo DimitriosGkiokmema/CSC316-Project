406981bb37cdb154bf270593668e95dca2e549be
Dimitrios <dimitrios.gkiokmema@mail.utoronto.ca>
2025-03-19 16:47:39 -0400

    Made line chart follow enter-update-exit

--- js/reportsLineGraph.js
+++ js/reportsLineGraph.js
@@ -1,43 +1,28 @@
-/*
-A line chart to display reports over time
-Points on the graph can be clicked to display a sidebar to show:
-    - Num Reports
-    - % increase from previous year
-    - Pie chart of shapes reported (use the proportionalPieChart class)
-    - Map of location breakdown (where those reports where made)
-    with circles representing the location. The bigger the circle,
-    the more sightings
-
-Ideas/Features:
-    - Draw the points that users should click on the graph, or let the 
-    entire line be able to trigger the sidebar
-*/
-
 class ReportsLineChartVis {
 
     constructor(parentElement, data) {
         this.parentElement = parentElement;
         this.data = data;
 
-        this.initVis()
+        this.initVis();
     }
 
     initVis() {
         let vis = this;
 
-        vis.margin = {top: 20, right: 20, bottom: 30, left: 55};
+        vis.margin = { top: 20, right: 20, bottom: 30, left: 55 };
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
-        // init drawing area
+        // Init drawing area
         vis.svg = d3.select("#" + vis.parentElement)
             .append("svg")
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
             .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
             .append("g")
-            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+            .attr('transform', `translate(${vis.margin.left}, ${vis.margin.top})`);
 
-        // add title
+        // Add title
         vis.svg.append('g')
             .attr('class', 'lineChartTitle')
             .attr('id', 'map-title')
@@ -52,20 +37,22 @@ class ReportsLineChartVis {
             .range([0, vis.width]);
         vis.yScale = d3.scaleLinear()
             .range([vis.height, 0]);
-        
-        // append tooltip
+
+        // Append tooltip
         vis.tooltip = d3.select("body").append('div')
             .attr('class', "tooltip")
             .style('opacity', 0);
-        
-        vis.wrangleData()
+
+        vis.wrangleData();
+        vis.createAxis();
+        vis.createLine();
     }
 
     wrangleData() {
         let vis = this;
 
         // Count occurrences of each year
-        let rawData = {}; 
+        let rawData = {};
 
         vis.data.forEach(d => {
             const year = +d.Date.getFullYear(); // Convert to number
@@ -78,7 +65,7 @@ class ReportsLineChartVis {
             value: value
         }));
 
-        let edges = {minYear: vis.lineData[0].year, maxYear: 0, minCount: 0, maxCount: 0};
+        let edges = { minYear: vis.lineData[0].year, maxYear: 0, minCount: 0, maxCount: 0 };
 
         vis.lineData.forEach(d => {
             // Finds min/max year
@@ -94,7 +81,7 @@ class ReportsLineChartVis {
             } else if (d.value < edges.minCount) {
                 edges.minCount = d.value;
             }
-        })
+        });
 
         // Set domain for scales
         vis.xScale.domain([edges.minYear, edges.maxYear]);
@@ -103,31 +90,28 @@ class ReportsLineChartVis {
         vis.updateVis();
     }
 
-    updateVis() {
-        this.createAxis();
-        this.createLine();
-        this.updateEmphasize();
-    }
-
     createAxis() {
         let vis = this;
-        
+
         var xAxis = d3.axisBottom(vis.xScale)
             .tickFormat(d => d);
         var yAxis = d3.axisLeft(vis.yScale);
 
         // Create x-axis
+        vis.svg.selectAll(".x-axis").remove(); // Remove existing x-axis
         vis.svg.append("g")
             .attr("class", "x-axis")
             .attr("transform", `translate(0,${vis.height})`)
             .call(xAxis);
 
         // Create y-axis
+        vis.svg.selectAll(".y-axis").remove(); // Remove existing y-axis
         vis.svg.append("g")
             .attr("class", "y-axis")
             .call(yAxis);
-        
+
         // X-axis label
+        vis.svg.selectAll(".lineChartAxisLabel").remove(); // Remove existing labels
         vis.svg.append('g')
             .attr('class', 'lineChartAxisLabel')
             .append('text')
@@ -146,7 +130,7 @@ class ReportsLineChartVis {
             .attr("y", -vis.margin.left / 2)
             .text("Number of Reports");
     }
-    
+
     createLine() {
         let vis = this;
 
@@ -154,6 +138,8 @@ class ReportsLineChartVis {
             .x(d => vis.xScale(d.year))
             .y(d => vis.yScale(d.value));
 
+        // Update or create the line
+        vis.svg.selectAll(".line").remove(); // Remove existing line
         vis.svg.append("path")
             .datum(vis.lineData)
             .attr("class", "line")
@@ -163,35 +149,53 @@ class ReportsLineChartVis {
             .attr("d", line);
     }
 
-    // Append an SVG circle on each x/y intersection 
-    updateEmphasize() {
+    updateVis() {
         let vis = this;
 
-        // Append circles at data points
-        vis.svg.selectAll("circle")
-            .data(vis.lineData)
-            .join("circle")
-            .attr("cx", d => vis.xScale(d.year))
-            .attr("cy", d => vis.yScale(d.value))
-            .attr("r", 2)
+        // Bind data to circles
+        const circles = vis.svg.selectAll("circle")
+            .data(vis.lineData, d => d.year); // Use year as key for data binding
+
+        // ENTER: Create new circles
+        circles.enter()
+            .append("circle")
+            .attr("class", "data-point")
+            .attr("r", 3) // Default radius
             .attr("fill", "#6DD0EB")
             .on("mouseover", (event, d) => {
+                // Highlight on hover
+                d3.select(event.currentTarget)
+                    .attr("r", 5) // Increase radius
+                    .attr("fill", "orange");
+
+                // Show tooltip
                 vis.tooltip.transition()
                     .duration(200)
-                    .style("opacity", .9);
-                vis.tooltip.html(d.year)
+                    .style("opacity", 1);
+                vis.tooltip.html(`Year: ${d.year}<br>Reports: ${d.value}`)
                     .style("left", (event.pageX + 5) + "px")
                     .style("top", (event.pageY - 28) + "px");
             })
             .on("mouseout", (event, d) => {
+                // Reset on mouseout
+                d3.select(event.currentTarget)
+                    .attr("r", 3) // Reset radius
+                    .attr("fill", "#6DD0EB");
+
+                // Hide tooltip
                 vis.tooltip.transition()
                     .duration(500)
                     .style("opacity", 0);
             })
             .on("click", (event, d) => vis.showSidebar(d))
+            .merge(circles)
+            .attr("cx", d => vis.xScale(d.year))
+            .attr("cy", d => vis.yScale(d.value));
+
+        // EXIT: Remove unused circles
+        circles.exit().remove();
     }
 
-    // Show details for a specific year in our datasets
     showSidebar(d) {
         let vis = this;
         let str = `<h3>${d.year}</h3> 
